e335c596431ff960301f3930dc692b8840be821d
alephnullptr <ben@alephnullptr.net>
2025-03-04 20:45:10 +0000

    Nicer styling and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Logo from "./assets/logo";
+
 export default function App() {
     return (
-        <div class="bg-gray-100 p-6 rounded-lg shadow-md">
-            <h1 class="text-2xl font-bold text-blue-600">AlephNullptr</h1>
-            <p class="mt-2 text-gray-700">Under construction</p>
+        <div class="p-6">
+            <div class="flex justify-center">
+                <Logo />
+            </div>
+            <h1 class="text-4xl text-center">AlephNullptr</h1>
+            <p class="mt-2 text-center">Under construction</p>
         </div>
     );
 }
